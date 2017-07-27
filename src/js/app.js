24ff96a928e3e53ab03ec0cd7b0f56bc254dea1c
larmflower <lnarosetta@gmail.com>
2017-07-27 11:58:41 +0100

    removed the CDN

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,3 @@
 angular
   .module('finalProject', ['ngResource', 'ui.router', 'satellizer', 'ngMessages', 'mhDragdropImage', 'ngAnimate'])
-  .constant('API_URL', 'http://localhost:3000');
+  .constant('API_URL', 'http://localhost:3000/api');
